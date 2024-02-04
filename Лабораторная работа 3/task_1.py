7dbf883d53e0e6fb3bfa48bcdd1bc1adbabe244f
ChaVi6 <90352382+ChaVi6@users.noreply.github.com>
2024-02-04 23:16:04 +0300

    Update task_1.py

--- Лабораторная работа 3/task_1.py
+++ Лабораторная работа 3/task_1.py
@@ -40,9 +40,6 @@ class PaperBook(Book):
             raise ValueError("Количество страниц меньше минимально допустимого значения")
         self._pages = pages
 
-    def __str__(self):
-        return f"Бумажная книга: {self.name}. Автор: {self.author}. Количество страниц: {self.pages}"
-
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
@@ -66,9 +63,6 @@ class AudioBook(Book):
         if duration < self.MIN_DURATION:
             raise ValueError("Продолжительность аудиокниги меньше минимально допустимого значения")
         self._duration = duration
-
-    def __str__(self):
-        return f"Аудиокнига: {self.name}. Автор: {self.author}. Продолжительность: {self.duration}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
